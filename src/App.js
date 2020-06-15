221e4b70cbe0b6f9cbc596c638b1940135617cdb
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2020-06-16 01:29:06 +0530

    react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Nav} from './components/Nav'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+
+export class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <Router>
+        <div className="route-container">
+          <Nav/>
+        </div>
+      </Router>
+    )
+  }
+}
+
+
+
+export default App;
