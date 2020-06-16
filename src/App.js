dda71a2e486a83d7570c234df3e067e0bbc99965
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2020-06-16 23:38:28 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import {Nav} from './components/Nav'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {AddTrip} from './components/trip/AddTrip'
+
 
 
 export class App extends React.Component{
   constructor(props){
     super(props)
-  }
+    this.state = {
+      allTrips:[
+        {
+          place: "Delhi",
+          date: "2040-4-4",
+          type : "Trek"
+        }
+      ]
+    }
+  };
 
   render(){
     return(
       <Router>
         <div className="route-container">
           <Nav/>
+          <Switch>
+            <Route path="/add" render={() => <AddTrip/>} 
+            />
+          </Switch>
         </div>
       </Router>
     )
---
+++ src/components/trip/AddTrip.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export const AddTrip = () => {
+    let date, place, type;
+    const submit = (e) => {
+        e.preventDefault()
+    }
+    return(
+        <div className="form-container">
+            <form onSubmit={submit} className="form black-container">
+                <label>
+                    <h3>Add a trip</h3>
+                    <br/>
+                    <br/>
+                    <br/>
+                </label>
+                <label>
+                    Date : <br/>
+                    <input id="date" type="date" required ref={(input) =>(date = input)} />
+                </label>
+
+                <label>
+                    Place : <br/>
+                    <input id="place" type="text" required ref={(input) =>(place = input)} />
+                </label>
+
+                <label>
+                    Type : <br/>
+                    <select ref={(input) => (type=input)}>
+                        <option value="Trek">Trek</option>
+                        <option value="Family Trip">Family Trip</option>
+                        <option value="Office Trip">Office Trip</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+};
